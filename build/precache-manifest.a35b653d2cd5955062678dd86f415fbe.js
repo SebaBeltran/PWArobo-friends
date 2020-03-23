a6c9902eee14f8983c22cd614a00fede2abcbfc9
Seba Beltran <sbeltran@unitedvirtualities.com>
2020-03-23 19:17:15 -0300

    First commit: Implemented Redux, Redux-Thunk, Code Splitting, PWA

---
+++ build/precache-manifest.a35b653d2cd5955062678dd86f415fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce901f48ede9f505b90b7cfaa612a382",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "9ca15218b180e135ffe3",
+    "url": "/robo-friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8921959a9ee0b5ff3f0f",
+    "url": "/robo-friends/static/css/main.e41f4f00.chunk.css"
+  },
+  {
+    "revision": "9ca15218b180e135ffe3",
+    "url": "/robo-friends/static/js/2.9776d892.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robo-friends/static/js/2.9776d892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8921959a9ee0b5ff3f0f",
+    "url": "/robo-friends/static/js/main.8696013e.chunk.js"
+  },
+  {
+    "revision": "0b1946ff84bf9c0a2139",
+    "url": "/robo-friends/static/js/runtime-main.31e5bff6.js"
+  }
+]);
